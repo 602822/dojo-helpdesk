876845f8cd9edbad245a67a5a1d9606e41960ada
vefjeld <vfj@vizrt.com>
2023-09-20 15:51:03 +0200

    added tailwind css

--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -2,11 +2,11 @@ import React from 'react'
 import Link from 'next/link'
 const NavBar = () => {
   return (
-    <nav>
-        <h1>Dojo Helpdesk</h1>
-        <Link href="/">Dashboard</Link>|
-        <Link href="/tickets">Tickets</Link>|
-        <Link href="/about">About</Link>
+    <nav className='bg-blue-500 p-4 text-white'>
+        <h1 className='text-2xl font-bold'>Dojo Helpdesk</h1>
+        <Link href="/" className="mx-2 hover:underline">Dashboard</Link>|
+        <Link href="/tickets" className="mx-2 hover:underline ">Tickets</Link>|
+        <Link href="/about" className="mx-2 hover:underline ">About</Link>
       </nav>
   )
 }
