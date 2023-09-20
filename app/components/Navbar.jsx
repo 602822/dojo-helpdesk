e10230eab1b11eaea69d5f8e84e28c1315ff4fed
vefjeld <vfj@vizrt.com>
2023-09-20 15:24:58 +0200

    navbar component

---
+++ app/components/Navbar.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const NavBar = () => {
+  return (
+    <nav>
+        <h1>Dojo Helpdesk</h1>
+        <Link href="/">Dashboard</Link>|
+        <Link href="/tickets">Tickets</Link>|
+        <Link href="/about">About</Link>
+      </nav>
+  )
+}
+
+export default NavBar
