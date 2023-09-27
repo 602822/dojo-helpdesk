299889cf6446a69424d57f8db1342063db0e2a70
vefjeld <vfj@vizrt.com>
2023-09-27 14:57:38 +0200

    you can click the tickets and view the details of the ticket

--- app/tickets/TicketList.jsx
+++ app/tickets/TicketList.jsx
@@ -1,21 +1,29 @@
 import React from "react";
+import Link from "next/link";
 async function getTickets() {
-  const res = await fetch("http://localhost:4000/tickets");
+  const res = await fetch("http://localhost:4000/tickets", {
+    next: {
+      revalidate: 0,
+    },
+  });
   return res.json();
 }
 
 export default async function TicketList() {
   const tickets = await getTickets();
+  const length = tickets.length;
   return (
     <>
       {tickets.map((ticket) => (
-        <div key={ticket.id} className="card my-5">
-          <h3>{ticket.title}</h3>
-          <p>{ticket.body.slice(0, 200)}...</p>
-          <div className={`pill ${ticket.priority}`}>
-            {ticket.priority} priority
+        <Link href={`/tickets/${ticket.id}`} key={ticket.id}>
+          <div key={ticket.id} className="card my-5">
+            <h3>{ticket.title}</h3>
+            <p>{ticket.body.slice(0, 200)}...</p>
+            <div className={`pill ${ticket.priority}`}>
+              {ticket.priority} priority
+            </div>
           </div>
-        </div>
+        </Link>
       ))}
       {tickets.length === 0 && (
         <p className="text-center"> There are no open tickets,</p>
