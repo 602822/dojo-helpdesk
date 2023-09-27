9fd5858ab655b47406bdef0ff27052d8586cdbf0
vefjeld <vfj@vizrt.com>
2023-09-27 13:54:34 +0200

    fetching the tickets from db.json

--- app/tickets/page.jsx
+++ app/tickets/page.jsx
@@ -1,11 +1,20 @@
-
-import React from 'react'
+import React from "react";
+import TicketList from "./TicketList";
 
 const Tickets = () => {
-  
   return (
-    <main>Tickets</main>
-  )
-}
+    <main>
+      <nav>
+        <div>
+          <h2>Tickets</h2>
+          <p>
+            <small>Currently open tickets.</small>
+          </p>
+        </div>
+      </nav>
+      <TicketList></TicketList>
+    </main>
+  );
+};
 
-export default Tickets
+export default Tickets;
